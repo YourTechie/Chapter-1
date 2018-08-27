88d94b7a49cb834b079291c8fccac6771f0c7840
Jordan Elliott <37377660+YourTechie@users.noreply.github.com>
2018-08-27 09:02:28 -0500

    New Project
    Chapter 1 Exercises

---
+++ Exercises/src/chapter_1_exercises/TableAndChairs.java
@@ -0,0 +1,10 @@
+package chapter_1_exercises;
+
+public class TableAndChairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
