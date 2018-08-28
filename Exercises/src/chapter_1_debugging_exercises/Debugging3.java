988803a7a6bc1be1f907a2bcc7d51ae34dd24800
Jordan Elliott <37377660+YourTechie@users.noreply.github.com>
2018-08-28 08:59:25 -0500

    Added new debugging package and debugging classes

---
+++ Exercises/src/chapter_1_debugging_exercises/Debugging3.java
@@ -0,0 +1,14 @@
+package chapter_1_debugging_exercises;
+
+public class Debugging3 {
+
+	 public static void main(String[] argz)
+	 {
+	 System.out.println("Over the river");
+     System.out.println("and through the woods");
+     System.out.println("to Grandmother's house we go");
+
+	
+	 }
+	
+}
---
+++ Exercises/src/chapter_1_debugging_exercises/Debugging4.java
@@ -0,0 +1,12 @@
+package chapter_1_debugging_exercises;
+
+import javax.swing.JOptionPane;
+
+public class Debugging4 {
+
+	 public static void main(String[] argz) {
+	
+	  JOptionPane.showMessageDialog(null, "First GUI program");
+
+	 }
+}
