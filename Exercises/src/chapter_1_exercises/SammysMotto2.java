277efe1e9a25a03c21f3441136f70fe7b7a471aa
Jordan Elliott <37377660+YourTechie@users.noreply.github.com>
2018-08-28 09:09:32 -0500

    Created SammysMotto classes

---
+++ Exercises/src/chapter_1_exercises/SammysMotto2.java
@@ -0,0 +1,16 @@
+package chapter_1_exercises;
+
+public class SammysMotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("S                              S");
+	    System.out.println("Sammy's makes it fun in the sun."); 
+		System.out.println("S                              S");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+	}
+
+}
